da868a0ca3aae59abd78aaa33be6366eb63b77e0
Y0hy0h <Y0hy0h@users.noreply.github.com>
2017-10-10 15:39:13 +0200

    Implement setting the URL

--- src/opencpu.spec.ts
+++ src/opencpu.spec.ts
@@ -1,7 +1,34 @@
-import { OpenCPU } from './opencpu';
+import { InvalidUrlError, OpenCPU } from './opencpu';
 
-describe('opencpu', () => {
+describe('ocpu', () => {
     it('should export class', () => {
         new OpenCPU();
     });
+
+    cases('#setUrl should sanitize url', (url, expectedError) => {
+        const opencpu = new OpenCPU();
+
+        if (expectedError === null) {
+            expect(() => opencpu.setUrl(url)).not.toThrow();
+            expect(opencpu['url'].toString()).toEqual(url);
+        } else {
+            expect(() => opencpu.setUrl(url)).toThrowError(TypeError);
+        }
+    }, [
+        ['localhost:5656/ocpu/library/statlets', null],
+        ['https://cloud.opencpu.org/ocpu/library/stocks', null],
+        ['', TypeError],
+        ['localhost:5656/ocpu/library', InvalidUrlError],
+    ]);
+
 });
+
+function cases(expectation: string, assertion, argList: Array<Array<any>>) {
+    describe(expectation, () => {
+        argList.forEach((args: Array<any>) => {
+            it(args.join(' | '), () => {
+                assertion.apply(this, args);
+            })
+        });
+    });
+}
--- src/opencpu.ts
+++ src/opencpu.ts
@@ -1,3 +1,17 @@
 export class OpenCPU {
+    private url: URL;
 
+    setUrl(url: string) {
+        const newUrl = new URL(url);
+        const validOpenCpuPath = /\/ocpu\/library\/.+$/;
+        const isValid = validOpenCpuPath.test(newUrl.pathname);
+        if (!isValid) {
+            throw new InvalidUrlError(`The URL ${newUrl} is not a valid OpenCPU path.\n` +
+                `It should follow the form '/ocpu/library/packagename'.\n` +
+                `Valid example: 'https://cloud.opencpu.org/ocpu/library/stocks'.`)
+        }
+        this.url = newUrl;
+    }
 }
+
+export class InvalidUrlError extends TypeError {}
