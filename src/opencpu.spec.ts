3a93f477945d9ccee1bb4b970b32589d56736ab6
Y0hy0h <Y0hy0h@users.noreply.github.com>
2017-10-11 17:10:44 +0200

    Refactor `OpenCPU` test

--- src/opencpu.spec.ts
+++ src/opencpu.spec.ts
@@ -1,29 +1,31 @@
-import { InvalidUrlError, OpenCPU } from './opencpu';
+import { OpenCPU } from './opencpu';
 
 describe('ocpu', () => {
-    it('should export class', () => {
-        new OpenCPU();
+    let opencpu: OpenCPU;
+    beforeEach(() => {
+        opencpu = new OpenCPU();
     });
 
-    cases('#setUrl should sanitize url', (url, expectedError) => {
-        const opencpu = new OpenCPU();
-
-        if (expectedError === null) {
-            expect(() => opencpu.setUrl(url)).not.toThrow();
-            expect(opencpu['url'].toString()).toEqual(url);
-        } else {
-            expect(() => opencpu.setUrl(url)).toThrowError(TypeError);
-        }
+    itCases('#setUrl should accept valid URLs', (url) => {
+        expect(() => opencpu.setUrl(url)).not.toThrow();
+        expect(opencpu['url'].href).toEqual(url);
     }, [
-        ['localhost:5656/ocpu/library/statlets', null],
-        ['https://cloud.opencpu.org/ocpu/library/stocks', null],
-        ['', TypeError],
-        ['localhost:5656/ocpu/library', InvalidUrlError],
+        ['localhost:5656/ocpu/library/statlets'],
+        ['https://cloud.opencpu.org/ocpu/library/stocks'],
     ]);
+
+    itCases('#setUrl should throw an error for invalid URLs', (url) => {
+            expect(() => opencpu.setUrl(url)).toThrowError(TypeError);
+        },
+        [
+            [''],
+            ['localhost:5656/ocpu/library'],
+        ]
+    );
 
 });
 
-function cases(expectation: string, assertion, argList: Array<Array<any>>) {
+function itCases(expectation: string, assertion, argList: Array<Array<any>>) {
     describe(expectation, () => {
         argList.forEach((args: Array<any>) => {
             it(args.join(' | '), () => {
