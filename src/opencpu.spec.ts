1bd3725a196009b159c0532bb8c0e27401986e22
Y0hy0h <y0hy0h@users.noreply.github.com>
2017-10-14 18:46:34 +0200

    Group tests

--- src/opencpu.spec.ts
+++ src/opencpu.spec.ts
@@ -11,51 +11,53 @@ describe('ocpu', () => {
         fetchMock.restore();
     });
 
-    itAsyncCases('#setUrl should accept valid URLs', (urlString, done) => {
+    describe('#setUrl', () => {
+        itAsyncCases('#setUrl should accept valid URLs', (urlString, done) => {
+                fetchMock.mock(urlString, 200);
+
+                opencpu.setUrl(urlString)
+                    .then(url => expect(url.href).toEqual(urlString))
+                    .catch(fail)
+                    .then(done);
+            }, [
+                ['localhost:5656/ocpu/library/statlets'],
+                ['https://cloud.opencpu.org/ocpu/library/stocks'],
+            ],
+        );
+
+        itAsyncCases('#setUrl should throw an error for invalid URLs', (urlString, done) => {
+                fetchMock.mock('*', 200);
+
+                opencpu.setUrl(urlString)
+                    .then(fail)
+                    .catch(error => expect(error).toEqual(jasmine.any(TypeError)))
+                    .then(done);
+            },
+            [
+                [''],
+                ['localhost:5656/ocpu/library'],
+            ],
+        );
+
+        it('#setUrl should succeed if url resonds with 200', (done) => {
+            const urlString = 'https://existantdomain.com/ocpu/library/packagename';
             fetchMock.mock(urlString, 200);
 
             opencpu.setUrl(urlString)
                 .then(url => expect(url.href).toEqual(urlString))
                 .catch(fail)
                 .then(done);
-        }, [
-            ['localhost:5656/ocpu/library/statlets'],
-            ['https://cloud.opencpu.org/ocpu/library/stocks'],
-        ],
-    );
+        });
 
-    itAsyncCases('#setUrl should throw an error for invalid URLs', (urlString, done) => {
-            fetchMock.mock('*', 200);
+        it('#setUrl should fail promise if url cannot be contacted', (done) => {
+            const urlString = 'https://nope.com/ocpu/library/packagename';
+            fetchMock.mock(urlString, () => {throw new TypeError("Failed to fetch");});
 
             opencpu.setUrl(urlString)
                 .then(fail)
-                .catch(error => expect(error).toEqual(jasmine.any(TypeError)))
+                .catch(error => expect(error).toEqual(new TypeError("Failed to fetch")))
                 .then(done);
-        },
-        [
-            [''],
-            ['localhost:5656/ocpu/library'],
-        ],
-    );
-
-    it('#setUrl should succeed if url resonds with 200', (done) => {
-        const urlString = 'https://existantdomain.com/ocpu/library/packagename';
-        fetchMock.mock(urlString, 200);
-
-        opencpu.setUrl(urlString)
-            .then(url => expect(url.href).toEqual(urlString))
-            .catch(fail)
-            .then(done);
-    });
-
-    it('#setUrl should fail promise if url cannot be contacted', (done) => {
-        const urlString = 'https://nope.com/ocpu/library/packagename';
-        fetchMock.mock(urlString, () => {throw new TypeError("Failed to fetch");});
-
-        opencpu.setUrl(urlString)
-            .then(fail)
-            .catch(error => expect(error).toEqual(new TypeError("Failed to fetch")))
-            .then(done);
+        });
     });
 });
 
@@ -82,6 +84,6 @@ function unwrapArgList(expectation: string, argList: Array<Array<any>>, itCall: 
     describe(expectation, () => {
         argList.forEach((args: Array<any>) => {
             itCall(args);
-        })
-    })
+        });
+    });
 }
