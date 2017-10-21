ddb4e124b4b122e4ef589b1d7862c18f9f80a939
Y0hy0h <y0hy0h@users.noreply.github.com>
2017-10-21 11:35:45 +0200

    Fix tests

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,8 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'src/*.ts'
+      'specs/**/*.ts',
+      'src/**/*.ts'
     ],
 
 
